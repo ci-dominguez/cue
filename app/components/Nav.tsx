aabc7f08854545e32c272cf6813b771fee7c98d9
Cristian <cdom127@wgu.edu>
2024-09-06 21:01:18 -0700

    Completed simple nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,10 +1,13 @@
+import { Link } from '@remix-run/react';
 import Logo from '../../public/imgs/logo.svg';
 
 const Nav = () => {
   return (
-    <nav className="p-6 pb-0 flex gap-x-2 bg-content text-text">
-      <img src={Logo} alt="Logo for RecMe" className="h-[2.5rem] w-auto" />
-      <h1 className="font-mono text-3xl">Rm</h1>
+    <nav className="p-6 flex bg-content text-text">
+      <Link to="/" className="flex gap-x-3">
+        <img src={Logo} alt="Logo for RecMe" className="h-[2.75rem] w-auto" />
+        <h1 className="font-mono text-3xl">Rm</h1>
+      </Link>
     </nav>
   );
 };
