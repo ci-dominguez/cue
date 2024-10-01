06c2a9c59fce59b20ce887457f8da5044664366b
ci-dominguez <c.isaacdominguez@gmail.com>
2024-10-01 15:05:32 -0700

    Add functionality to query for recommendations

--- app/utils/tmdb.ts
+++ app/utils/tmdb.ts
@@ -11,6 +11,7 @@ const searchResultSchema = z.object({
   title: z.string(),
   release_date: z.string().nullable(),
   media_type: z.enum(["movie", "tv"]),
+  poster_path: z.string().nullable(),
 });
 
 export type SearchResult = z.infer<typeof searchResultSchema>;
@@ -47,5 +48,45 @@ export async function searchTMDb(q: string): Promise<SearchResult[]> {
   } catch (error) {
     console.error("Error fetching search results from TMDb:", error);
     throw new Error("Failed to fetch search results from TMDb");
+  }
+}
+
+export async function getRecommendations(
+  id: number,
+  mediaType: "movie" | "tv"
+): Promise<SearchResult[]> {
+  try {
+    const resp = await axios.get(
+      `${TMDB_BASE_URL}/${mediaType}/${id}/recommendations`,
+      {
+        params: {
+          api_key: TMDB_API_KEY,
+          language: "en-US",
+          page: 1,
+        },
+      }
+    );
+
+    const res = resp.data.results
+      .map((item: any) => {
+        try {
+          return searchResultSchema.parse({
+            ...item,
+            title: item.title || item.name,
+            media_type: mediaType,
+          });
+        } catch (error) {
+          console.error("Failed to parse recommendation item:", item, error);
+          return null;
+        }
+      })
+      .filter(
+        (item: SearchResult | null): item is SearchResult => item !== null
+      );
+
+    return res;
+  } catch (error) {
+    console.error("Error fetching recommendations from TMDb:", error);
+    throw new Error("Failed to fetch recommendations from TMDb");
   }
 }
