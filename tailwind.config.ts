7384859e4cb21ce16f4cad36e85c16b219d05786
ci-dominguez <c.isaacdominguez@gmail.com>
2024-11-08 13:03:11 -0800

    Update font and color configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,16 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        mono: ['"Roboto Mono"', 'monospace'],
+        lora: ['"Lora"', 'sans'],
+        inter: ['"Inter"', 'sans'],
       },
       colors: {
-        text: '#E2E6E8',
-        content: '#151515',
-        accent: '#8FFF00',
+        content: '#FFF',
+        'content-1': '#F5F5F4',
+        'content-2': '#D9D9D9',
+        text: '#0C0A09',
+        'text-1': '#44403C',
+        accent: '#FDF3DD',
       },
     },
   },
