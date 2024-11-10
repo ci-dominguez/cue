b15d318a7e25f7d488cd1e4fe3a97b9fb6260113
ci-dominguez <c.isaacdominguez@gmail.com>
2024-11-10 10:25:47 -0800

    Create a nav context

---
+++ app/contexts/NavContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from 'react';
+
+interface NavContextProps {
+  extended: boolean;
+  setExtended: (val: boolean) => void;
+}
+
+const NavContext = createContext<NavContextProps | undefined>(undefined);
+
+export const NavProvider = ({ children }: { children: React.ReactNode }) => {
+  const [extended, setExtended] = useState(false);
+
+  return (
+    <NavContext.Provider value={{ extended, setExtended }}>
+      {children}
+    </NavContext.Provider>
+  );
+};
+
+export const useNav = () => {
+  const context = useContext(NavContext);
+  if (!context) throw new Error('UseNav must be used within a NavProvider');
+  return context;
+};
