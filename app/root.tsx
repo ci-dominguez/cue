8d99a187d1274b4082002c93a60b753ab17c3180
ci-dominguez <c.isaacdominguez@gmail.com>
2024-11-10 10:27:46 -0800

    Update root imports

--- app/root.tsx
+++ app/root.tsx
@@ -6,34 +6,35 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import './tailwind.css';
-import Nav from './components/Nav';
+import Nav from './components/navigation/Nav';
 import Footer from './components/Footer';
+import { NavProvider } from './contexts/NavContext';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap'
+          rel='stylesheet'
         />
         <Meta />
         <Links />
       </head>
       <body>
-        <Nav />
-        {children}
-        <Footer />
-        <ScrollRestoration />
-        <Scripts />
+        <NavProvider>
+          <Nav />
+          <main className='flex flex-col p-6 min-h-screen bg-content text-text font-inter'>
+            {children}
+          </main>
+          <Footer />
+          <ScrollRestoration />
+          <Scripts />
+        </NavProvider>
       </body>
     </html>
   );
