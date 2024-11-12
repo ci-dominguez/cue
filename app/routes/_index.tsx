540548e2bfc64f3fa39a41fee1585f94cd78ba8b
ci-dominguez <c.isaacdominguez@gmail.com>
2024-11-11 16:47:36 -0800

    Updated ui + wip to migrating search to its own page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useLoaderData, useNavigate, useSearchParams } from '@remix-run/react';
+import {
+  Link,
+  useLoaderData,
+  useNavigate,
+  useSearchParams,
+} from '@remix-run/react';
 import { LoaderFunction, json } from '@remix-run/node';
 import {
   getMovieDetails,
@@ -11,6 +16,9 @@ import {
 import Search from '~/components/Search';
 import Recommendations from '~/components/Recommendations';
 import type { MetaFunction } from '@remix-run/node';
+import Button from '~/components/ui/Button';
+import { useNav } from '~/contexts/NavContext';
+import { ThumbsUp, Zap, Popcorn } from 'lucide-react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -91,6 +99,7 @@ export default function Index() {
   );
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const { setExtended } = useNav();
 
   useEffect(() => {
     // Reset selectedItem when URL parameters are cleared
@@ -107,26 +116,95 @@ export default function Index() {
   };
 
   return (
-    <main className="px-6 pt-12 flex flex-col bg-content text-text min-h-screen font-mono">
-      <h1 className="mx-auto text-center text-3xl pb-4">Give me recs for...</h1>
+    // <main className='px-6 pt-12 flex flex-col bg-content text-text min-h-screen font-lora'>
+    //   <h1 className='mx-auto text-center text-4xl pb-4 font-medium'>
+    //     Give me recs for...
+    //   </h1>
 
-      <Search onItemSelect={handleItemSelect} />
+    //   <Search onItemSelect={handleItemSelect} />
 
-      {selectedItem && (
-        <div className="mt-20">
-          <h2 className="text-2xl mb-4 mx-auto text-center">
-            Best recs for people that like{' '}
-            <span className="text-accent">
-              {selectedItem.title || selectedItem.name}
-            </span>
-          </h2>
-          <Recommendations
-            recommendations={recommendations}
-            isLoading={false}
-            error={error}
-          />
+    //   {selectedItem && (
+    //     <div className='mt-20'>
+    //       <h2 className='text-2xl mb-4 mx-auto text-center'>
+    //         Best recs for people that like{' '}
+    //         <span className='text-accent'>
+    //           {selectedItem.title || selectedItem.name}
+    //         </span>
+    //       </h2>
+    //       <Recommendations
+    //         recommendations={recommendations}
+    //         isLoading={false}
+    //         error={error}
+    //       />
+    //     </div>
+    //   )}
+    // </main>
+    <>
+      <div className='flex flex-col space-y-4 px-6'>
+        <h1 className='font-lora font-semibold text-4xl'>
+          Find What to Watch Next,{' '}
+          <span className='bg-accent'>Effortlessly</span>
+        </h1>
+        <p>
+          Discover hidden gems and trending shows tailored to your tastes.
+          Cue&apos;s smart recommendations make finding something great to watch
+          easier than ever.
+        </p>
+        <Button variant='default'>
+          <Link
+            to='/search'
+            onClick={() => {
+              setExtended(false);
+            }}
+            className='py-2.5 px-6'
+          >
+            Start Searching 🔍
+          </Link>
+        </Button>
+        <Button variant='secondary'>
+          <Link
+            to='/about'
+            onClick={() => {
+              setExtended(false);
+            }}
+            className='py-2.5 px-6'
+          >
+            Learn More
+          </Link>
+        </Button>
+      </div>
+
+      <div className='flex flex-col space-y-4 py-10 px-6 mt-10 bg-content-1'>
+        <h2 className='font-lora font-semibold text-2xl'>Why choose Cue?</h2>
+        <div className='py-4 grid gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+          <div className='flex flex-col space-y-2 border-gray-800 rounded-lg'>
+            <ThumbsUp className='h-12 w-12 text-primary' />
+            <h3 className='text-xl font-lora font-medium'>
+              Personalized Recommendations
+            </h3>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Get movie suggestions tailored to your unique taste and viewing
+              history.
+            </p>
+          </div>
+
+          <div className='flex flex-col space-y-2 border-gray-800 rounded-lg'>
+            <Zap className='h-12 w-12 text-primary' />
+            <h3 className='text-xl font-lora font-medium'>Quick and Easy</h3>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Find your next movie in seconds with our intuitive interface.
+            </p>
+          </div>
+
+          <div className='flex flex-col space-y-2 border-gray-800 rounded-lg'>
+            <Popcorn className='h-12 w-12 text-primary' />
+            <h3 className='text-xl font-lora font-medium'>Diverse Selection</h3>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Explore a wide range of genres, from blockbusters to indie gems.
+            </p>
+          </div>
         </div>
-      )}
-    </main>
+      </div>
+    </>
   );
 }
