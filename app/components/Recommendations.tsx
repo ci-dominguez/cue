2ad7ffa6db1f35dae2f2e333d8a5a507d6526748
Cristian <cdom127@wgu.edu>
2024-11-12 19:32:17 -0800

    Update ui for recommendation gallery and cards. Favoriting wip.

--- app/components/Recommendations.tsx
+++ app/components/Recommendations.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@remix-run/react';
+import { Heart, Star } from 'lucide-react';
 import { SearchResult } from '~/utils/tmdb';
 
 interface RecommendationsProps {
@@ -13,43 +14,45 @@ const Recommendations = ({
   error,
 }: RecommendationsProps) => {
   if (isLoading) {
-    return <div className="text-center py-4">Loading recommendations...</div>;
+    return <div className='text-center py-4'>Loading recommendations...</div>;
   }
 
   if (error) {
-    return <div className="text-center py-4 text-red-500">{error}</div>;
+    return <div className='text-center py-4 text-red-500'>{error}</div>;
   }
 
   if (recommendations.length === 0) {
-    return <div className="text-center py-4">No recommendations found.</div>;
+    return <div className='text-center py-4'>No recommendations found.</div>;
   }
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-10 mt-8 max-w-7xl mx-auto">
+    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-10 max-w-7xl mx-auto'>
       {recommendations.map((item) => (
         <Link
           key={item.id}
           to={`/${item.media_type}s/${item.id}`}
-          className="overflow-hidden flex flex-col"
+          className='overflow-hidden flex flex-col'
         >
           {item.poster_path && (
-            <div className="aspect-w-2 aspect-h-3">
+            <div className='aspect-w-2 aspect-h-3'>
               <img
                 src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
                 alt={item.title}
-                className="w-full h-full object-cover"
+                className='w-full h-full object-cover rounded'
               />
             </div>
           )}
-          <div className="flex flex-col flex-grow pt-2">
-            <h3 className="text-lg mb-2 flex-grow">{item.title}</h3>
-            <div className="flex justify-between mt-auto">
-              <p className="text-sm text-gray-500">
-                {item.media_type === 'movie' ? 'Movie' : 'TV Show'}
+          <div className='flex flex-col flex-grow pt-2'>
+            <h3 className='text-xl mb-2 flex-grow font-lora font-semibold hover:underline'>
+              {item.title}
+            </h3>
+
+            <div className='flex justify-between mt-auto font-bold'>
+              <p className='text-sm text-content-2 flex items-center space-x-1.5'>
+                <Star className='size-5 fill-orange-500 stroke-orange-500' />
+                <span>{item.vote_average.toString().slice(0, 3)}</span>
               </p>
-              <p className="text-sm text-gray-600 text-right">
-                {item.release_date && new Date(item.release_date).getFullYear()}
-              </p>
+              <Heart className='size-5' />
             </div>
           </div>
         </Link>
