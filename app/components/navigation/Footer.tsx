c64bfbf2778cc2556bd36cd103ae15479b5634f6
Cristian <cdom127@wgu.edu>
2024-11-20 18:33:55 -0800

    Update footer to be centered on wider devices

--- app/components/navigation/Footer.tsx
+++ app/components/navigation/Footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
   const route = useLocation();
 
   return (
-    <footer className='px-6 py-4 flex flex-row justify-between bg-content'>
-      <ul className='flex flex-col font-inter font-medium text-md mb-6'>
-        <li className='text-text font-bold pb-4'>Tools</li>
-        {links.slice(0, 3).map((i) => {
-          return (
+    <footer className='px-6 py-4 bg-content'>
+      <div className='flex flex-col sm:flex-row justify-center items-start sm:items-start gap-8 max-w-4xl mx-auto'>
+        <ul className='flex flex-col font-inter font-medium text-md mb-6'>
+          <li className='text-text font-bold pb-4'>Tools</li>
+          {links.slice(0, 3).map((i) => (
             <li
               key={i.title}
               className='text-center flex items-center justify-between py-1 space-x-1.5'
@@ -31,14 +31,12 @@ const Footer = () => {
                 <div className='size-1 rounded-full bg-text' />
               )}
             </li>
-          );
-        })}
-      </ul>
+          ))}
+        </ul>
 
-      <ul className='flex flex-col font-inter font-medium text-md mb-6'>
-        <li className='text-text font-bold pb-4'>Resources</li>
-        {links.slice(3).map((i) => {
-          return (
+        <ul className='flex flex-col font-inter font-medium text-md mb-6'>
+          <li className='text-text font-bold pb-4'>Resources</li>
+          {links.slice(3).map((i) => (
             <li
               key={i.title}
               className='text-center flex items-center justify-between py-1 space-x-1.5'
@@ -58,25 +56,25 @@ const Footer = () => {
                 <div className='size-1 rounded-full bg-text' />
               )}
             </li>
-          );
-        })}
-        <li className='text-center flex items-center justify-between py-1 space-x-1.5'>
-          <Link
-            to='https://cidominguez.com/'
-            onClick={() => setExtended(false)}
-            className={
-              route.pathname === 'https://cidominguez.com/'
-                ? 'text-text'
-                : 'text-orange-400'
-            }
-          >
-            @cidominguez
-          </Link>
-          {route.pathname === 'https://cidominguez.com/' && (
-            <div className='size-1 rounded-full bg-text' />
-          )}
-        </li>
-      </ul>
+          ))}
+          <li className='text-center flex items-center justify-between py-1 space-x-1.5'>
+            <Link
+              to='https://cidominguez.com/'
+              onClick={() => setExtended(false)}
+              className={
+                route.pathname === 'https://cidominguez.com/'
+                  ? 'text-text'
+                  : 'text-orange-400'
+              }
+            >
+              @cidominguez
+            </Link>
+            {route.pathname === 'https://cidominguez.com/' && (
+              <div className='size-1 rounded-full bg-text' />
+            )}
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
