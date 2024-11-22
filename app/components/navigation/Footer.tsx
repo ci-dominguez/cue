6c0e9e2f282b84598999c9c56b6a00fbbd96b112
Cristian <cdom127@wgu.edu>
2024-11-22 12:36:59 -0800

    Update footer layout

--- app/components/navigation/Footer.tsx
+++ app/components/navigation/Footer.tsx
@@ -1,79 +1,51 @@
 import { useNav } from '~/contexts/NavContext';
 import { Link, useLocation } from '@remix-run/react';
 import { links } from '~/utils/links';
+import Logo from '../../../imgs/logo.svg';
 
 const Footer = () => {
   const { setExtended } = useNav();
   const route = useLocation();
 
   return (
-    <footer className='px-6 py-4 bg-content'>
-      <div className='flex flex-col sm:flex-row justify-center items-start sm:items-start gap-8 max-w-4xl mx-auto'>
-        <ul className='flex flex-col font-inter font-medium text-md mb-6'>
-          <li className='text-text font-bold pb-4'>Tools</li>
-          {links.slice(0, 3).map((i) => (
-            <li
-              key={i.title}
-              className='text-center flex items-center justify-between py-1 space-x-1.5'
-            >
-              <Link
-                to={i.href}
-                onClick={() => setExtended(false)}
-                className={
-                  route.pathname === i.href
-                    ? 'text-text'
-                    : 'text-text-1 opacity-80'
-                }
-              >
-                {i.title}
-              </Link>
-              {route.pathname === i.href && (
-                <div className='size-1 rounded-full bg-text' />
-              )}
-            </li>
-          ))}
+    <footer className='max-w-screen-xl mx-auto px-6 sm:px-10 md:px-12 lg:px-16 pb-6 sm:pb-10 md:pb-12 lg:pb-18 flex flex-col'>
+      <Link
+        to='/'
+        onClick={() => setExtended(false)}
+        className='flex gap-x-3 items-center'
+      >
+        <img src={Logo} alt='Logo for RecMe' className='h-[2rem] w-auto' />
+        <h1 className='font-inter font-bold text-xl'>Cue</h1>
+      </Link>
+      <div className='flex flex-col gap-3 mt-3'>
+        <ul>
+          <li className='text-lg font-bold pb-1'>Tools</li>
+          {links.map((link) => {
+            return (
+              <li key={link.title}>
+                <Link
+                  to={link.href}
+                  className={` text-lg hover:underline hover:font-medium ${
+                    route.pathname === link.href
+                      ? 'text-text font-semibold underline'
+                      : 'text-text-1 font-normal'
+                  }`}
+                >
+                  <span>{link.title}</span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-
-        <ul className='flex flex-col font-inter font-medium text-md mb-6'>
-          <li className='text-text font-bold pb-4'>Resources</li>
-          {links.slice(3).map((i) => (
-            <li
-              key={i.title}
-              className='text-center flex items-center justify-between py-1 space-x-1.5'
-            >
-              <Link
-                to={i.href}
-                onClick={() => setExtended(false)}
-                className={
-                  route.pathname === i.href
-                    ? 'text-text'
-                    : 'text-text-1 opacity-80'
-                }
-              >
-                {i.title}
-              </Link>
-              {route.pathname === i.href && (
-                <div className='size-1 rounded-full bg-text' />
-              )}
-            </li>
-          ))}
-          <li className='text-center flex items-center justify-between py-1 space-x-1.5'>
-            <Link
-              to='https://cidominguez.com/'
-              onClick={() => setExtended(false)}
-              className={
-                route.pathname === 'https://cidominguez.com/'
-                  ? 'text-text'
-                  : 'text-orange-400'
-              }
-            >
-              @cidominguez
-            </Link>
-            {route.pathname === 'https://cidominguez.com/' && (
-              <div className='size-1 rounded-full bg-text' />
-            )}
-          </li>
-        </ul>
+        <span className='text-lg'>
+          Made by{' '}
+          <Link
+            to='https://cidominuguez.com'
+            className='underline font-semibold hover:font-bold text-orange-500'
+          >
+            @cidominguez
+          </Link>
+        </span>
       </div>
     </footer>
   );
