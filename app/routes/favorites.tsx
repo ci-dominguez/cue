8200ccc3e9aa096dca63272bd6ef3974d892674d
Cristian <cdom127@wgu.edu>
2024-11-22 13:42:27 -0800

    Update page layouts to be consistent with new ui

--- app/routes/favorites.tsx
+++ app/routes/favorites.tsx
@@ -11,18 +11,23 @@ const FavoritesPage = () => {
   }, []);
   return (
     <>
-      <div className='flex flex-col space-y-10 text-text pb-10 px-6'>
-        <h1 className='font-lora font-semibold text-4xl'>Your Favorites</h1>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-10 max-w-7xl mx-auto'>
-          {favs.length > 0 ? (
-            favs.map((i) => {
-              return <PosterCard variant='alt' key={i.id} item={i} />;
-            })
-          ) : (
-            <>No favorites</>
-          )}
+      <section className='w-full max-w-screen-2xl mx-auto pb-6 sm:pb-10 md:pb-12 lg:pb-18'>
+        <div className='w-full max-w-screen-xl mx-auto px-8 sm:px-10 md:px-12 lg:px-16'>
+          <h1 className='pb-4 font-lora font-semibold tracking-tight text-4xl md:text-5xl lg:text-6xl max-w-[18ch]'>
+            Your Favorites
+          </h1>
+
+          <div className='pt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-10 max-w-7xl mx-auto'>
+            {favs.length > 0 ? (
+              favs.map((i) => {
+                return <PosterCard variant='alt' key={i.id} item={i} />;
+              })
+            ) : (
+              <>No favorites</>
+            )}
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
--- app/routes/recently-viewed.tsx
+++ app/routes/recently-viewed.tsx
@@ -12,29 +12,34 @@ const RecentsPage = () => {
   }, []);
   return (
     <>
-      <div className='flex flex-col space-y-10 text-text pb-10 px-6'>
-        <h1 className='font-lora font-semibold text-4xl'>Recently Viewed</h1>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-10 max-w-7xl mx-auto'>
-          {recents.length > 0 ? (
-            recents.map((i) => {
-              return (
-                <PosterCard
-                  variant='alt'
-                  key={i.id}
-                  item={{
-                    ...i,
-                    title: i.title ?? 'Untitled',
-                    release_date: i.release_date ?? 'Not Released',
-                    poster_path: i.poster_path ?? '',
-                  }}
-                />
-              );
-            })
-          ) : (
-            <>No favorites</>
-          )}
+      <section className='w-full max-w-screen-2xl mx-auto pb-6 sm:pb-10 md:pb-12 lg:pb-18'>
+        <div className='w-full max-w-screen-xl mx-auto px-8 sm:px-10 md:px-12 lg:px-16'>
+          <h1 className='pb-4 font-lora font-semibold tracking-tight text-4xl md:text-5xl lg:text-6xl max-w-[18ch]'>
+            Recently Viewed
+          </h1>
+
+          <div className='pt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-10 max-w-7xl mx-auto'>
+            {recents.length > 0 ? (
+              recents.map((i) => {
+                return (
+                  <PosterCard
+                    variant='alt'
+                    key={i.id}
+                    item={{
+                      ...i,
+                      title: i.title ?? 'Untitled',
+                      release_date: i.release_date ?? 'Not Released',
+                      poster_path: i.poster_path ?? '',
+                    }}
+                  />
+                );
+              })
+            ) : (
+              <>No favorites</>
+            )}
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
