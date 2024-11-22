50498a45a231223e5bb7817d8c92d2c0f3f183ed
Cristian <cdom127@wgu.edu>
2024-11-22 13:01:42 -0800

    Update hover states for interactivity

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -5,15 +5,15 @@ interface ButtonProps {
 }
 const Button = ({ children, variant = 'default', className }: ButtonProps) => {
   const variantStylesMap: Record<string, string> = {
-    default: 'bg-text text-content',
-    secondary: 'bg-content-1 text-text-1',
+    default: 'bg-text text-content hover:bg-accent hover:text-text',
+    secondary: 'bg-content-1 text-text-1 hover:bg-content-2 hover:text-text',
   };
 
   const variantStyles =
     variantStylesMap[variant] || variantStylesMap['default'];
 
   const baseStyles =
-    'flex group not-prose self-start items-center justify-center rounded-md text-sm font-semibold';
+    'flex group not-prose self-start items-center justify-center rounded-md text-sm lg:text-base font-semibold';
 
   return (
     <button className={`${baseStyles} ${variantStyles} ${className}`}>
